7673e99c13113dcbf85e20230c2a39c41fa3cc8d
Andy Dziabo <jameszabo@yahoo.com>
2022-08-11 08:46:24 -0400

    commit tree b73f52536dbbed3fac8bf37221f4d097015f2beb

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity){
+    if(activity !== undefined){
+        return `This Saturday, I want to ${activity}!`;
+    }else{
+        return "This Saturday, I want to roller-skate!";
+    }
+}
+function mondayWork(job){
+    let monday;
+    if(job !== undefined){
+        monday = job;
+    }else{
+        monday = "go to the office";
+    }
+    return `This Monday, I will ${monday}.`;
+}
+function wrapAdjective(flare = '*'){
+    return function(compliment = "special"){
+        return `You are ${flare}${compliment}${flare}!`;
+    }
+}
